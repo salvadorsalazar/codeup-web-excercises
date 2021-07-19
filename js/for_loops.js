3e4eb9b1ac1b288219d8118daa0e1a0f84d89f16
salvador salazar <salvador.g.salazar79@gmail.com>
2021-07-18 22:12:16 -0500

    adding planets arrays exercises

--- js/for_loops.js
+++ js/for_loops.js
@@ -11,34 +11,46 @@
 //         }
 // }
 // showMultiplicationTable();
+
 //====================================
-// function showMultiplicationTable(x) {
-//
-//     for (i = 1; i <= 10; i++)
-//     {
-//         console.log(x + ' x ' + i + " = " + (x * i));
-//     }
-// }
-//
+
+/*
+function showMultiplicationTable(x) {
+
+    for (i = 1; i <= 10; i++)
+    {
+        console.log(x + ' x ' + i + " = " + (x * i));
+    }
+}
+*/
+
 // //==================
-// function OddOrEven(x){
-//     for(i=1;i<=10;i++){
-//         var randomNumber = Math.floor(Math.random() * 20)+80;
-//         if (randomNumber %2 ===0){
-//             console.log('number :' + randomNumber + " is Even")
-//
-//         }
-//         if(randomNumber %2 !== 0){
-//             console.log("number : " + randomNumber + " is Odd")
-//
-//
-//         }
-//     }
-//    // console.log(i)
-// }
-//
-// OddOrEven();
+
+/*
+function OddOrEven(x){
+
+    for(i=1;i<=10;i++){
+        var randomNumber = Math.floor(Math.random() * 180)+20;
+        if (randomNumber %2 ===0){
+            console.log('number :' + randomNumber + " is Even")
+
+        }
+        if(randomNumber %2 !== 0){
+            console.log("number : " + randomNumber + " is Odd")
+
+
+        }
+    }
+   // console.log(i)
+}
+
+OddOrEven();
+
+*/
+
+
 //=============================
+
 // Create a for loop that uses console.log to create the output shown below.
 //
 //
@@ -54,27 +66,44 @@
 //======================================
 //chrisinassoultion
 //=====================
-// function numPayramid(x){
-//
-//     for( count = 1;count <= x ;count++){
-//         var stringOutput = "";
-//         for(duration = 0;duration < count;duration++){
-//             stringOutput = stringOutput + count.toString();
-//                     }
-//         console.log(stringOutput);
-//     }
-// }
-// numPayramid(9)
+/*
+function numPayramid(x){
+
+    for( count = 1;count <= x ;count++){
+        var stringOutput = "";
+        for(duration = 0;duration < count;duration++){
+            stringOutput = stringOutput + count.toString();
+                    }
+        console.log(stringOutput);
+    }
+}
+numPayramid(9)
+
 //=========================================
+ */
 
 
+var crazyOne = 1;
+for (i = 1; i < 10; i++) {
+    console.log(i * crazyOne);
+    crazyOne  = crazyOne.toString() + 1;
+}
 
+/*
 function decreaseNumbers(x){
 
+
     for (i = 100; i >=0; i--){
+      // i = i -5;
         console.log(i);
+        i = i -5;
+
     }
+    // for (i = 100; i >=0; i--){
+    //     console.log(i);
+    // }
 
 }
 decreaseNumbers();
 
+*/
