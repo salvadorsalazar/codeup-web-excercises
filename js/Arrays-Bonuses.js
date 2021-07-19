1d61252de0dde8a1af8a96b097e3537c9b9eab03
salvador salazar <salvador.g.salazar79@gmail.com>
2021-07-19 12:15:53 -0500

    array mainpilation-lecture.html

---
+++ js/Arrays-Bonuses.js
@@ -0,0 +1,11 @@
+// Define a function named allIndexesOf that takes in two arguments. The first argument should be the array to search and the second argument should
+// be the value you want to search for. If the item does not exist in the provided array, return an empty array.
+//
+//     Given:
+//
+// var fruits = ["apple", "banana", "orange", "apple", "pineapple"];
+// allIndexesOf(fruits, "apple") should return the array [0, 3]
+// allIndexesOf(fruits, "guava") should return the array []
+// allIndexesOf(fruits, "pineapple") should return [4]
+
+function allInde
