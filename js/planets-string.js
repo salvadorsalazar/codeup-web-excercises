0d8c2e7078cb937cd83208a608b1bb3bc0ad9193
salvador salazar <salvador.g.salazar79@gmail.com>
2021-07-18 22:51:51 -0500

    adding planets_strings exercises

--- js/planets-string.js
+++ js/planets-string.js
@@ -10,8 +10,8 @@
      * planetsArray.
      * console.log planetsArray to check your work
      */
-
-    // console.log(planetsArray);
+     planetsArray = planetsString.split('|');
+     console.log(planetsArray);
 
     /**
      * TODO:
@@ -23,5 +23,9 @@
      * list. You will need an opening AND closing <ul> tags around the entire
      * string, and <li> tags around each planet.
      */
+
+   // var planetsBreak ="Mercury|Venus|Earth|Mars|Jupiter|Saturn|Uranus|Neptune"
+    var newPlanetsString = planetsArray.join('\n');
+    console.log(newPlanetsString);
 
 })();
